a02f74a2cf236bcdc546e147a123f99baeed2cfe
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-19 20:14:09 +0700

    Update Indomie help.py

--- BdrlMusic/modules/Indomie help.py
+++ BdrlMusic/modules/Indomie help.py
@@ -47,37 +47,16 @@ Penjelasan:
            Untuk melepas pembisuan anggota dari grup</b>
 """,
 ]
-help_next = [
-    f"""<b>Haloo</b>
-    
-    """,
-]
-help_previous = [
-    f"""<b>Haloo</b>
-    
-    """,
-]
 help_limit = [
-    f"""<b>HELP LIMIT 
+    f"""<b>HELP LIMIT
     
-Perintah:
-        <code>{PREFIXES[0]}limit</code> [Burung Puyuh]
-Penjelasan:
-        Gaada penjelasan, pasti kalian udh tau kan panteq?
-        <b> kau ketik aja </b> <code>{PREFIXES[0]}limit</code> nanti juga bisa cok!</b>
-    """,
-
-]
-help_play = [
-   f"""<b>HELP PLAY
+ Perintah:
+        <code>{PREFIXES[0]}limit</code> [ketik saja limit]
+ Penjelasan: 
+        Gaperlu gw jelasin lupasti tau</b>
     
-Perintah:
-        <code>{PREFIXES[0]}play</code> [Judul lagu]
-Penjelasan:
-        Silahkan ketik <code>{PREFIXES[0]}play</code> beserta judul lagu</b>
     """,
 ]
-
 help_sticker = [
     f"""<b>HELP STICKER KANG
 
@@ -131,8 +110,7 @@ help_text = {
     "memes": help_sticker[2],
     "quotly": help_sticker[3],
     "tiny": help_sticker[4],
-    "limit": help_limit[0],
-    "play": help_play[0],
+    "limit": help_limit[5],
 }
 
 
@@ -154,26 +132,22 @@ async def _(client, inline_query):
     button = [
         [
             InlineKeyboardButton("Globals", callback_data="admin admin_gban"),
-            InlineKeyboardButton("Kangcut", callback_data="sticker sticker_kang"),
+            InlineKeyboardButton("Kang", callback_data="sticker sticker_kang"),
         ],
         [
             InlineKeyboardButton("Memify", callback_data="sticker sticker_memify"),
-            InlineKeyboardButton("Memes", callback_data="sticker sticker_memes"),
+            InlineKeyboardButton("Mames", callback_data="sticker sticker_memes"),
         ],
         [
             InlineKeyboardButton("Quotly", callback_data="sticker sticker_quotly"),
-            InlineKeyboardButton("Restrict", callback_data="admin admin_restrict"),
+            InlineKeyboardButton("Admin", callback_data="admin admin_restrict"),
         ],
         [
             InlineKeyboardButton("Tiny", callback_data="sticker sticker_tiny"),
             InlineKeyboardButton("Limit", callback_data="limit limit"),
         ],
-        [
-            InlineKeyboardButton("<", callback_data="next next_nih"),
-            InlineKeyboardButton(">", callback_data="previous previous_nih"),
-        ],
     ]
-    msg = "<b>HELP MENU OPEN\nPREFIXES: <code>NothingBot</code></b>"
+    msg = "<b>HELP MENU OPEN\nPREFIXES: <code>. , : ; !</code></b>"
     await client.answer_inline_query(
         inline_query.id,
         cache_time=0,
@@ -195,7 +169,7 @@ async def _(client, callback_query):
         if callback_query.from_user.id == my.me.id:
             data = callback_query.data.split()[1]
             button = [
-                [InlineKeyboardButton("• Mbalik •", callback_data="admin admin_back")]
+                [InlineKeyboardButton("• KEMBALI •", callback_data="admin admin_back")]
             ]
             if data == "admin_gban":
                 msg = help_admin[0]
@@ -203,26 +177,38 @@ async def _(client, callback_query):
                 msg = help_admin[1]
             if data == "admin_back":
                 button = [
-        [
-            InlineKeyboardButton("Globals", callback_data="admin admin_gban"),
-            InlineKeyboardButton("Kangcut", callback_data="sticker sticker_kang"),
-        ],
-        [
-            InlineKeyboardButton("Memify", callback_data="sticker sticker_memify"),
-            InlineKeyboardButton("Memes", callback_data="sticker sticker_memes"),
-        ],
-        [
-            InlineKeyboardButton("Quotly", callback_data="sticker sticker_quotly"),
-            InlineKeyboardButton("Restrict", callback_data="admin admin_restrict"),
-        ],
-        [
-            InlineKeyboardButton("Tiny", callback_data="sticker sticker_tiny"),
-            InlineKeyboardButton("Limit", callback_data="limit limit"),
-        ],
-        [
-            InlineKeyboardButton("<", callback_data="next next_nih"),
-            InlineKeyboardButton(">", callback_data="previous previous_nih"),
-        ],
+                    [
+                        InlineKeyboardButton(
+                            "Globals", callback_data="admin admin_gban"
+                        ),
+                        InlineKeyboardButton(
+                            "Kang", callback_data="sticker sticker_kang"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Memify", callback_data="sticker sticker_memify"
+                        ),
+                        InlineKeyboardButton(
+                            "Mames", callback_data="sticker sticker_memes"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Quotly", callback_data="sticker sticker_quotly"
+                        ),
+                        InlineKeyboardButton(
+                            "Admin", callback_data="admin admin_restrict"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Tiny", callback_data="sticker sticker_tiny"
+                        ),
+                             InlineKeyboardButton(
+                            "Limit", callback_data="limit limit"
+                        ),
+                    ],
                 ]
                 msg = "<b>HELP MENU OPEN\nPREFIXES: <code>. , : ; !</code></b>"
             await callback_query.edit_message_text(
@@ -238,7 +224,7 @@ async def _(client, callback_query):
             button = [
                 [
                     InlineKeyboardButton(
-                        "• Mbalik •", callback_data="sticker sticker_back"
+                        "• KEMBALI •", callback_data="sticker sticker_back"
                     )
                 ]
             ]
@@ -252,68 +238,44 @@ async def _(client, callback_query):
                 msg = help_sticker[3]
             if data == "sticker_tiny":
                 msg = help_sticker[4]
+            if data == "limit":
+                msg = help_limit[5]
             if data == "sticker_back":
                 button = [
                     [
-            InlineKeyboardButton("Globals", callback_data="admin admin_gban"),
-            InlineKeyboardButton("Kangcut", callback_data="sticker sticker_kang"),
-        ],
-        [
-            InlineKeyboardButton("Memify", callback_data="sticker sticker_memify"),
-            InlineKeyboardButton("Memes", callback_data="sticker sticker_memes"),
-        ],
-        [
-            InlineKeyboardButton("Quotly", callback_data="sticker sticker_quotly"),
-            InlineKeyboardButton("Restrict", callback_data="admin admin_restrict"),
-        ],
-        [
-            InlineKeyboardButton("Tiny", callback_data="sticker sticker_tiny"),
-            InlineKeyboardButton("Limit", callback_data="limit limit"),
-        ],
-        [
-            InlineKeyboardButton("<", callback_data="next next_nih"),
-            InlineKeyboardButton(">", callback_data="previous previous_nih"),
-        ],
-                ]
-                msg = "<b>HELP MENU OPEN\nPREFIXES: <code>. , : ; !</code></b>"
-            await callback_query.edit_message_text(
-                msg, reply_markup=InlineKeyboardMarkup(button)
-            )
-@bot.on_callback_query(filters.regex("^limit"))
-async def _(client, callback_query):
-    for my in ubot._ubot:
-        if callback_query.from_user.id == my.me.id:
-            data = callback_query.data.split()[1]
-            button = [
-                [InlineKeyboardButton("• Mbalik •", callback_data="limit limit_back")]
-            ]
-            if data == "limit":
-                msg = help_limit[0]   
-            if data == "limit_back":
-                button = [
+                        InlineKeyboardButton(
+                            "Globals", callback_data="admin admin_gban"
+                        ),
+                        InlineKeyboardButton(
+                            "Kang", callback_data="sticker sticker_kang"
+                        ),
+                    ],
                     [
-            InlineKeyboardButton("Globals", callback_data="admin admin_gban"),
-            InlineKeyboardButton("Kangcut", callback_data="sticker sticker_kang"),
-        ],
-        [
-            InlineKeyboardButton("Memify", callback_data="sticker sticker_memify"),
-            InlineKeyboardButton("Memes", callback_data="sticker sticker_memes"),
-        ],
-        [
-            InlineKeyboardButton("Quotly", callback_data="sticker sticker_quotly"),
-            InlineKeyboardButton("Restrict", callback_data="admin admin_restrict"),
-        ],
-        [
-            InlineKeyboardButton("Tiny", callback_data="sticker sticker_tiny"),
-            InlineKeyboardButton("Limit", callback_data="limit limit"),
-        ],
-        [
-            InlineKeyboardButton("<", callback_data="next next_nih"),
-            InlineKeyboardButton(">", callback_data="previous previous_nih"),
-        ],
+                        InlineKeyboardButton(
+                            "Memify", callback_data="sticker sticker_memify"
+                        ),
+                        InlineKeyboardButton(
+                            "Mames", callback_data="sticker sticker_memes"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Quotly", callback_data="sticker sticker_quotly"
+                        ),
+                        InlineKeyboardButton(
+                            "Admin", callback_data="admin admin_restrict"
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Tiny", callback_data="sticker sticker_tiny"
+                        ),
+                        InlineKeyboardButton(
+                            "Limit", callback_data="limit limit"
+                        ),
+                    ],
                 ]
                 msg = "<b>HELP MENU OPEN\nPREFIXES: <code>. , : ; !</code></b>"
             await callback_query.edit_message_text(
                 msg, reply_markup=InlineKeyboardMarkup(button)
             )
-
