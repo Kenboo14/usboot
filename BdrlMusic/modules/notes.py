7decba9eedb641efe48f46ec03bc54e4ccf1030f
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-22 01:44:01 +0700

    Update notes.py

--- BdrlMusic/modules/notes.py
+++ BdrlMusic/modules/notes.py
@@ -1,1 +1,10 @@
+print("Install notes.py")
+from inspect import getfullargspec
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from BdrlMusic import ubot
+from config import PREFIXES
+
 print "DANA 088088008"
