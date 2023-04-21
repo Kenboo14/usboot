134ead8bea2e5840178f82f3d557cf6c702c0906
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-22 03:48:31 +0700

    Update misc.py

--- BdrlMusic/modules/misc.py
+++ BdrlMusic/modules/misc.py
@@ -6,7 +6,11 @@ from pyrogram.types import Message
 from BdrlMusic import ubot
 from BdrlMusic.utils import dbfunctions as TOMI
 from config import PREFIXES
+from pyrogram import *
+from pyrogram.types import *
+
 from BdrlMusic import *
+from config import *
 
 log = []
 
@@ -134,7 +138,7 @@ async def _(client, message):
         except Exception as error:
             return await message.reply(error)
     else:
-        if message.command[1] in help_text:
+        if message.command[1] in payment_text:
             return await message.reply(payment_text[message.command[1]]) 
          
          
