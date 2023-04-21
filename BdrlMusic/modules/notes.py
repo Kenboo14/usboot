624b292c9a9c438e27f48438d22b714d05847860
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-22 01:50:32 +0700

    Update notes.py

--- BdrlMusic/modules/notes.py
+++ BdrlMusic/modules/notes.py
@@ -1,10 +1,26 @@
-print("Install notes.py")
-from inspect import getfullargspec
+import asyncio
 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.raw.functions.messages import DeleteHistory, StartBot
 
 from BdrlMusic import ubot
 from config import PREFIXES
 
-print "DANA 088088008"
+
+@ubot.on_message(filters.command("notes", PREFIXES) & filters.me)
+async def _(client, message):
+    await client.unblock_user("Kippubot")
+    bot_info = await client.resolve_peer("Kippubot")
+    msg = await message.reply("<code>Processing . . .</code>")
+    response = await client.invoke(
+        StartBot(
+            bot=bot_info,
+            peer=bot_info,
+            random_id=client.rnd_id(),
+            start_param="start",
+        )
+    )
+    await asyncio.sleep(1)
+    status = await client.get_messages("Kippubot", response.updates[1].message.id + 1)
+    await msg.edit(status.text)
+    return await client.invoke(DeleteHistory(peer=bot_info, max_id=0, revoke=True))
