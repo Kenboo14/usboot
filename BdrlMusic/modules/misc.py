58e28be18a780317f77f6bd034d168c1d7f9b76b
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-23 00:31:32 +0700

    Update misc.py

--- BdrlMusic/modules/misc.py
+++ BdrlMusic/modules/misc.py
@@ -171,8 +171,7 @@ async def _(client, inline_query):
         ],
     ]
 
-   msg = "<b>HELP MENU OPEN\nSUPPORT BY SEEKUT CORP: <code>. , : ; !</code></b>"
-   await client.answer_inline_query(
+   msg = "<b>HELP MENU OPEN\nSUPPORT BY SEEKUT CORP: <code>. , : ; !</code></b>"await client.answer_inline_query(
         inline_query.id,
         cache_time=0,
         results=[
