13e057e3af3423875b179a75bdf235aa650c82e6
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-23 00:13:05 +0700

    Update misc.py

--- BdrlMusic/modules/misc.py
+++ BdrlMusic/modules/misc.py
@@ -171,23 +171,21 @@ async def _(client, inline_query):
         ],
     ]
 
-
-msg = "<b>HELP MENU OPEN\nSUPPORT BY SEEKUT CORP: <code>. , : ; !</code></b>"
-
-await client.answer_inline_query(
-    inline_query.id,
-    cache_time=0,
-    results=[
-        (
-            InlineQueryResultArticle(
-                title="Help Menu!",
-                reply_markup=InlineKeyboardMarkup(button),
-                input_message_content=InputTextMessageContent(msg),
+   msg = "<b>HELP MENU OPEN\nSUPPORT BY SEEKUT CORP: <code>. , : ; !</code></b>"
+ 
+   await client.answer_inline_query(
+        inline_query.id,
+        cache_time=0,
+        results=[
+            (
+                InlineQueryResultArticle(
+                    title="Help Menu!",
+                    reply_markup=InlineKeyboardMarkup(button),
+                    input_message_content=InputTextMessageContent(msg),
+                )
             )
-        )
-    ],
-)
-
+        ],
+    )
 
 @bot.on_callback_query(filters.regex("^payment"))
 async def _(client, callback_query):
