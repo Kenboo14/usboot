96fbdd66037f6e0224ac3f0780d0002f281e831a
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-22 13:28:10 +0700

    Update misc.py

--- BdrlMusic/modules/misc.py
+++ BdrlMusic/modules/misc.py
@@ -7,12 +7,6 @@ from BdrlMusic import ubot
 from BdrlMusic.utils import dbfunctions as TOMI
 from config import PREFIXES
 
-from pyrogram import *
-from pyrogram.types import *
-
-from BdrlMusic import *
-from config import *
-
 log = []
 
 __MODULE__ = "NOTES"
@@ -36,24 +30,7 @@ Perintah:
          <code>{PREFIXES[0]}notes</code>
 Penjelasan:
            Untuk melihat daftar catatan yang disimpan 
-""",
-help_payment = [
-    f"""
-<b>INI ADALAH PAYMENT PEMBAYARAN KENBO
-Penjelasan:
-           Payment kenbo hanya BCA & DANA
-           """,
-     f"""
-<b>INI ADALAH PAY PEMBAYARAN KENBO
-Penjelasan:
-           Payment kenbo hanya BCA & DANA
-           """,
-]
-
-payment_text = {
-         "paymont": help_payment[0],
-         "payment": help_payment[1],
-}
+"""
 
 
 @ubot.on_message(filters.command(["stats", "status"], PREFIXES) & filters.me)
@@ -128,20 +105,10 @@ async def _(client, message):
         f"👆🏻 Pesan diatas ini jangan dihapus\n\n👉🏻 Ketik: <code>{PREFIXES[0]}get {note_name}</code>",
     )
     await message.reply(f"Note <code>{note_name}</code> berhasil di simpan")
-         
-     @ubot.on_message(filters.me & filters.command("get", PREFIXES))
-async def _(client, message):
-     if len(message.command) < 2:
-        x = await client.get_inline_bot_results(bot.me.username, "user_get_command")
-        try:
-            return await message.reply_inline_bot_result(x.query_id, x.results[0].id)
-        except Exception as error:
-            return await message.reply(error)
-    else:
-        if message.command[1] in payment_text:
-            return await message.reply(payment_text[message.command[1]])
 
-    
+
+@ubot.on_message(filters.command("get", PREFIXES) & filters.me)
+async def _(client, message):
     note_name = get_arg(message)
     if not note_name:
         return await message.reply("lo goblok atau gimana?")
@@ -160,94 +127,7 @@ async def _(client, message):
             message.chat.id, client.me.id, note, reply_to_message_id=message.id
         )
 
- 
 
-@bot.on_inline_query(filters.regex("^user_get_command"))
-async def _(client, inline_query):
-    button = [
-        [
-            InlineKeyboardButton("DANA", callback_data="payment paymont"),
-            InlineKeyboardButton("BCA", callback_data="payment payment"),
-        ],
-             
-    ]
-    msg = "<b>PAYMENT\nINI ADALAH PEMBAYARAN KENBO\nSUPPORT BY SEEKUT CORP></b>"
-    await client.answer_inline_query(
-        inline_query.id,
-        cache_time=0,
-        results=[
-            (
-                InlineQueryResultArticle(
-                    title="Help Menu!",
-                    reply_markup=InlineKeyboardMarkup(button),
-                    input_message_content=InputTextMessageContent(msg),
-                )
-            )
-        ],
-    )
-             
-@bot.on_callback_query(filters.regex("^payment"))
-async def _(client, callback_query):
-    for my in ubot._ubot:
-        if callback_query.from_user.id == my.me.id:
-            data = callback_query.data.split()[1]
-            button = [
-                [InlineKeyboardButton("• KEMBALI •", callback_data="payment payment_back")]
-            ]
-            if data == "paymont":
-                msg = help_payment[0]
-            if data == "admin_restrict":
-                msg = help_payment[1]
-            if data == "payment_back":
-                button = [
-                    [
-                        InlineKeyboardButton(
-                            "Global", callback_data="admin admin_gban"
-                        ),
-                        InlineKeyboardButton(
-                            "Kang", callback_data="sticker sticker_kang"
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "Memify", callback_data="sticker sticker_memify"
-                        ),
-                        InlineKeyboardButton(
-                            "Mames", callback_data="sticker sticker_memes"
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "Quotly", callback_data="sticker sticker_quotly"
-                        ),
-                        InlineKeyboardButton(
-                            "Admin", callback_data="admin admin_restrict"
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "Tiny", callback_data="sticker sticker_tiny"
-                        ),
-                        InlineKeyboardButton(
-                            "Limit", callback_data="sticker limit"
-                        ),
-                    ],                                     
-                    [                       
-                         InlineKeyboardButton(
-                            "Play", callback_data="sticker play"
-                        ),
-                        InlineKeyboardButton(
-                            ">>", callback_data="next next"
-                        ),
-                    ],
-                ]
-            msg = "<b>HELP MENU OPEN\nSUPPORT BY SEEKUT CORP: <code>. , : ; !</code></b>"    
-            await callback_query.edit_message_text(
-                msg, reply_markup=InlineKeyboardMarkup(button)
-            )
-             
-             
-             
 @ubot.on_message(filters.command("delnote", PREFIXES) & filters.me)
 async def _(client, message):
     note_name = get_arg(message)
