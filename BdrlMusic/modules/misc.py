64d016a5620438dc6b578fbb143c132cac19e4e8
Genta14045 <116920242+Genta14045@users.noreply.github.com>
2023-04-23 21:14:32 +0700

    Update misc.py

--- BdrlMusic/modules/misc.py
+++ BdrlMusic/modules/misc.py
@@ -168,7 +168,7 @@ async def _(client, inline_query):
         [
             InlineKeyboardButton("DANA", callback_data="payment paymont"),
         ],
-    msg = ""
+    msg = "Payment Pembayaran"
     await client.answer_inline_query(
     inline_query.id,
     text="Hello",
@@ -177,7 +177,7 @@ async def _(client, inline_query):
         [
             InlineKeyboardButton("BCA", callback_data="payment payment"),
         ],
-    msg = ""
+    msg = "Payment Pembayarans"
     await client.answer_inline_query(
     inline_query.id,
     text="anbu",
