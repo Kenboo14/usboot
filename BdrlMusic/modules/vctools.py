164d1650438510f87a71650de33f62f70f063a4a
Kenboox <116920242+Kenboo14@users.noreply.github.com>
2023-04-25 13:40:09 +0700

    Update vctools.py

--- BdrlMusic/modules/vctools.py
+++ BdrlMusic/modules/vctools.py
@@ -34,32 +34,6 @@ async def get_group_call(
     await message.edit(f"**No group call Found** {err_msg}")
     return False
 
-def get_arg(message: Message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
-
-
-def get_args(message: Message):
-    try:
-        message = message.text
-    except AttributeError:
-        pass
-    if not message:
-        return False
-    message = message.split(maxsplit=1)
-    if len(message) <= 1:
-        return []
-    message = message[1]
-    try:
-        split = shlex.split(message)
-    except ValueError:
-        return message
-    return list(filter(lambda x: len(x) > 0, split))
-
 @Client.on_message(filters.command(["startvc"], PREFIXES) & filters.me)
 async def opengc(client: Client, message: Message):
     flags = " ".join(message.command[1:])
