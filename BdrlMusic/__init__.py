077cd091fb590c449706b995de4766cbd2bb954b
Kenboox <116920242+Kenboo14@users.noreply.github.com>
2023-04-26 22:32:32 +0700

    Update __init__.py

--- BdrlMusic/__init__.py
+++ BdrlMusic/__init__.py
@@ -1,13 +1,8 @@
-import asyncio
-import logging
-import sys
 import time
 from datetime import datetime
 from logging import *
 from typing import Callable
-from typing import Any, Dict
 
-from logging.handlers import RotatingFileHandler
 from pyrogram.enums import ParseMode
 from aiohttp import ClientSession
 from pyrogram import Client
@@ -18,38 +13,6 @@ from rich.logging import RichHandler
 from pytgcalls import *
 
 from config import *
-
-
-LOG_FILE_NAME = "logs.txt"
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s] - %(name)s - %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
-        logging.StreamHandler(),
-    ],
-)
-logging.getLogger("asyncio").setLevel(logging.CRITICAL)
-logging.getLogger("pytgcalls").setLevel(logging.WARNING)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
-logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
-
-LOGS = logging.getLogger(__name__)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
-
-
-if not API_ID:
-    LOGGER(__name__).error("No API_ID Found! Exiting!")
-    sys.exit()
-
-if not API_HASH:
-    LOGGER(__name__).error("No API_HASH Found! Exiting!")
-    sys.exit()
 
 
 CMD_HELP = {}
@@ -122,5 +85,4 @@ ubot = Ubot(
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_STRING,
-    plugins=dict(root="BdrlMusic/modules"),
 )
