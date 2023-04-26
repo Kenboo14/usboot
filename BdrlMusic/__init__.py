6d5145558303c442ac38864cca032ac044334400
Kenboox <116920242+Kenboo14@users.noreply.github.com>
2023-04-26 20:29:41 +0700

    Update __init__.py

--- BdrlMusic/__init__.py
+++ BdrlMusic/__init__.py
@@ -1,17 +1,68 @@
+import asyncio
+import logging
+import sys
 import time
 from datetime import datetime
 from logging import *
 from typing import Callable
+from typing import Any, Dict
 
+from logging.handlers import RotatingFileHandler
 from aiohttp import ClientSession
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 from pyrogram.handlers import MessageHandler
 from pytgcalls import PyTgCalls
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyromod import listen
 from rich.logging import RichHandler
+from pytgcalls import GroupCallFactory
 
 from config import *
+
+
+LOG_FILE_NAME = "logs.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+
+LOGS = logging.getLogger(__name__)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+
+if (
+    not STRING_SESSION
+):
+    LOGGER(__name__).error("No String Session Found! Exiting!")
+    sys.exit()
+
+if not API_ID:
+    LOGGER(__name__).error("No API_ID Found! Exiting!")
+    sys.exit()
+
+if not API_HASH:
+    LOGGER(__name__).error("No API_HASH Found! Exiting!")
+    sys.exit()
+
+if BOTLOG_CHATID:
+    BOTLOG_CHATID = BOTLOG_CHATID
+else:
+    BOTLOG_CHATID = "me"
+
 
 CMD_HELP = {}
 ids = []
@@ -38,7 +89,6 @@ bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    parse_mode=ParseMode.HTML,
 )
 
 
@@ -83,4 +133,11 @@ ubot = Ubot(
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_STRING,
+    plugins=dict(root="BdrlMusic/modules"),
 )
+
+bots = [bot for bot in [ubot] if bot]
+
+for bot in bots:
+    if not hasattr(bot, "group_call"):
+        setattr(bot, "group_call", GroupCallFactory(bot).get_group_call())
